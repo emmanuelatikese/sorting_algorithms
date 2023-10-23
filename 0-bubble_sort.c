63b1221df6c57e2fee7038f2cb6c04eb9fd78a7b
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-23 05:01:45 -0700

    adding

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,9 +11,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
-	size_t s_num = 1;
+	size_t s_num = 1, a = 0;
 
-	size_t a = 0;
+	if (array == NULL | size < 2)
+		return;
 
 	while (s_num != 0)
 	{
