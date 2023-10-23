0afa3ce93f183b0377d0bd2620ee42246f05a87a
emmanuelatikese <emmanuelatikese14@gmail.com>
2023-10-23 04:44:31 -0700

    adding bubbles

---
+++ 0-bubble_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+/**
+ * bubble_sort - this is a sorting algorithm
+ *
+ * @array: this is an int parameter
+ * @size: this is a size parameter
+ *
+ * Return: It returns nothing or void
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	int tmp;
+	size_t s_num = 1;
+
+	size_t a = 0, b = 1;
+
+	while (s_num != 0)
+	{
+		s_num = 0;
+
+		for (a = 0; a < size - 1; a++)
+		{
+			for (b = a + 1; b < size; b++)
+			{
+				if (array[a] > array[b])
+				{
+					tmp = array[a];
+					array[a] = array[b];
+					array[b] = tmp;
+					s_num += 1;
+					print_array(array, size);
+				}
+			}
+		}
+	}
+}
